200e70b860c2960a936b8982332c627ff8eef798
unknown <krystian1wojtowicz@gmail.com>
2020-06-19 10:38:31 +0200

    Updates

---
+++ precache-manifest.43c98196a5101e9c5d3dac2baf5c38c3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "815c69d5f9b7a21f4edb",
+    "url": "/routing/./static/css/main.124b988d.chunk.css"
+  },
+  {
+    "revision": "815c69d5f9b7a21f4edb",
+    "url": "/routing/./static/js/main.815c69d5.chunk.js"
+  },
+  {
+    "revision": "cf7a9b9b6f513f435513",
+    "url": "/routing/./static/js/runtime~main.cf7a9b9b.js"
+  },
+  {
+    "revision": "6dde60d9131975f245e6",
+    "url": "/routing/./static/js/2.6dde60d9.chunk.js"
+  },
+  {
+    "revision": "6c3c53dca8f3726451698aa623d16395",
+    "url": "/routing/./static/media/header1.6c3c53dc.jpg"
+  },
+  {
+    "revision": "28f9c40aece528aefb261f8a396d5aee",
+    "url": "/routing/./static/media/header3.28f9c40a.jpg"
+  },
+  {
+    "revision": "e9cb24ae078e458d716111d520240162",
+    "url": "/routing/./static/media/header2.e9cb24ae.jpg"
+  },
+  {
+    "revision": "0d8770fa272ccc7b1d32eaf12bab272e",
+    "url": "/routing/./index.html"
+  }
+];
